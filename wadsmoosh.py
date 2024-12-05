b4830c900a6868aef7ea8316bc2514ac46895a37
vanessakindell <vanessakindell@gmail.com>
2024-12-04 21:14:52 -0500

    Fixed copyright notice per Owlet7

--- wadsmoosh.py
+++ wadsmoosh.py
@@ -1,3 +1,6 @@
+##-----------------------------------------------------------------------------
+##
+## Copyright 2024 Owlet VII, Vanessa Kindell 
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -14,25 +17,27 @@
 ##
 ##-----------------------------------------------------------------------------
 ##
+
 ##
-## This code is derived from WadSmoosh 1.41, which is covered by the following permissions:
+## This code is derived from wadsmoosh-freedoom, which is covered by the following permissions:
 ##
 ##------------------------------------------------------------------------------------------
 ##
 ## The MIT License (MIT)
-##
-## Copyright (c) 2016-2023 JP LeBreton
-##
+## 
+## Copyright (c) 2016-2024 JP LeBreton
+## Copyright (c) 2023-2024 Exequiel Mleziva
+## 
 ## Permission is hereby granted, free of charge, to any person obtaining a copy
 ## of this software and associated documentation files (the "Software"), to deal
 ## in the Software without restriction, including without limitation the rights
 ## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ## copies of the Software, and to permit persons to whom the Software is
 ## furnished to do so, subject to the following conditions:
-##
+## 
 ## The above copyright notice and this permission notice shall be included in
 ## all copies or substantial portions of the Software.
-##
+## 
 ## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -43,8 +48,6 @@
 ##
 ##------------------------------------------------------------------------------------------
 ##
-
-## Support for Legacy of Rust from Wadfusion by Owlet VII
 
 import os, sys, time
 from shutil import copyfile
